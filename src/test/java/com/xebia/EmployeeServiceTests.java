1a14f099ecb173ab5b765c5938bc5ccd531e69dd
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-08-24 01:21:20 +0530

    created test for employee get

---
+++ src/test/java/com/xebia/EmployeeServiceTests.java
@@ -0,0 +1,17 @@
+package com.xebia;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class EmployeeServiceTests {
+    @Test
+    public void getEmployeesReturnsOK() throws Exception {
+        HttpResponse response = Request.Get("http://localhost:8080/spring-boot-microservice-1.0/api/employees").execute().returnResponse();
+        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+}
