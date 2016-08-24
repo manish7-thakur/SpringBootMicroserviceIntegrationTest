b253aed42a6554ac7c7b91c86ebf557b48bb1c1e
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-08-24 10:54:54 +0530

    Added time delay

--- src/test/java/com/xebia/EmployeeServiceTests.java
+++ src/test/java/com/xebia/EmployeeServiceTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class EmployeeServiceTests {
     @Test
     public void getEmployeesReturnsOK() throws Exception {
+        Thread.sleep(20000);
         HttpResponse response = Request.Get("http://localhost:8080/spring-boot-microservice-1.0/api/employees").execute().returnResponse();
         Assert.assertEquals(200, response.getStatusLine().getStatusCode());
     }
