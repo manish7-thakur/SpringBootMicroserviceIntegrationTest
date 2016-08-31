5f811a14a94ed47cea9acfecbd51651e4c5e7654
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-08-31 09:18:13 +0530

    Read port from system variable

--- src/test/java/com/xebia/EmployeeServiceTests.java
+++ src/test/java/com/xebia/EmployeeServiceTests.java
@@ -11,8 +11,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class EmployeeServiceTests {
     @Test
     public void getEmployeesReturnsOK() throws Exception {
+        String port = System.getenv("PORT");
+        if(port == null) port = "8080";
         Thread.sleep(20000);
-        HttpResponse response = Request.Get("http://localhost:8080/spring-boot-microservice-1.0/api/employees").execute().returnResponse();
+        HttpResponse response = Request.Get("http://localhost:"+ port +"/spring-boot-microservice-1.0/api/employees").execute().returnResponse();
         Assert.assertEquals(200, response.getStatusLine().getStatusCode());
     }
 }
